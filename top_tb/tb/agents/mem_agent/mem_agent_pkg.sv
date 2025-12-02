dcca5c0f7c14a80e25c5a1b787fdb295fa6457e4
wweqqll <liwenqi2001@outlook.com>
2025-12-02 19:31:02 +0100

    fix mem_agent

--- top_tb/tb/agents/mem_agent/mem_agent_pkg.sv
+++ top_tb/tb/agents/mem_agent/mem_agent_pkg.sv
@@ -5,9 +5,9 @@ import common::*;
 `include "uvm_macros.svh"
 
 `include "mem_seq_item.svh"
+`include "mem_agent_config.svh"
 `include "mem_monitor.svh"
 `include "mem_agent.svh"
-`include "mem_agent_config.svh"
 
 
 endpackage
